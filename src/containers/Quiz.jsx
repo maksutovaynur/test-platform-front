cea61dd71bd52499234d9f91213b69a247e1c383
Aynur Maksutov <maksutov_aynur@mail.ru>
2020-12-13 08:49:32 +0300

    selected question

--- src/containers/Quiz.jsx
+++ src/containers/Quiz.jsx
@@ -13,16 +13,26 @@ async function getQuizData(quizId) {
     return data;
 }
 
+function putAnswer(quiz_id, question_key, answer) {
+    localStorage.setItem(`${quiz_id}/${question_key}`, answer);
+}
+
+function getAnswer(quiz_id, question_key) {
+    return localStorage.getItem(`${quiz_id}/${question_key}`) || undefined;
+}
 
 function Question (props) {
-    const {number, question_text, options} = props;
+    const {number, question_text, options, setAnswer, answerGiven, question_key} = props;
     return (<div>
         <h2>#{number}</h2>
         <div dangerouslySetInnerHTML={{__html: question_text}}/>
         <div>{
             Object.keys(options).map(k => {
                 const o = options[k];
-                return (<div className="q-option">
+                var selected = false;
+                if (answerGiven === k) selected = true;
+                const qClass = "q-option" + (selected ? " q-option-selected" : "");
+                return (<div className={qClass} onClick={() => setAnswer(question_key, k)}>
                     <div className="symb">{k}{")"}</div>
                     <div dangerouslySetInnerHTML={{__html: o}}/>
                 </div>)
@@ -43,10 +53,29 @@ class Quiz extends React.Component {
         const { quizId } = urlParams;
 
         getQuizData(quizId).then((result) => {
-            this.setState({questions: result})
+            this.setState({questions: result}, this.fixQuestions)
         });
     }
     componentDidMount() {
+    }
+
+    fixQuestions = () => {
+        const urlParams = this.props.match.params;
+        const { quizId } = urlParams;
+        this.setState(({questions}) => {
+            const newQuestions = questions.map(q => ({
+                ...q,
+                answerGiven: getAnswer(quizId, q.question_key)
+            }))
+            return {questions: newQuestions}
+        })
+    }
+
+    setAnswer = (qk, a) => {
+        const urlParams = this.props.match.params;
+        const { quizId } = urlParams;
+        putAnswer(quizId, qk, a);
+        this.setState({[`Q${qk}`]: a}, this.fixQuestions);
     }
 
     render() {
@@ -54,9 +83,16 @@ class Quiz extends React.Component {
         // const { quizId } = urlParams;
         // const searchParams = new URLSearchParams(this.props.location.search);
         const {questions} = this.state;
+        const urlParams = this.props.match.params;
+        const { quizId } = urlParams;
 
         return (<div>
-            {questions.map((k, i) => (<Question key={i} number={i + 1} {...k}/>))}
+            <h1>{quizId}</h1>
+            {questions.map((k, i) => (<Question
+                key={i}
+                setAnswer={this.setAnswer}
+                number={i + 1} {...k}
+            />))}
         </div>);
     }
 };
